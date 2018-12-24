7d42b08dbeac5133a4e722a240badb17535693b0
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2018-12-24 12:46:42 -0500

    Updated simpWallet to work around current bugs with Plutus/Playground

--- Simple/simpWallet.hs
+++ Simple/simpWallet.hs
@@ -1,17 +1,19 @@
--- This is an empty SC with the most basic setup needed to get started.
-
+-- SimpWallet is an empty SC allowing anyone to deposit and withdraw all funds from the SC (assuming their wallet is registered to track the SC before others deposit)
 module SimpWallet where
 
 import qualified Language.PlutusTx            as PlutusTx
-import qualified Language.PlutusTx.Prelude    as P
 import           Ledger
-import           Ledger.Validation
 import           Wallet
 import           Playground.Contract
 
 
 walletValidator :: ValidatorScript
-walletValidator = Ledger.emptyValidator
+walletValidator = ValidatorScript $ Ledger.fromCompiledCode
+                  $$(PlutusTx.compile
+                     [||
+                     -- Due to what appears to be a bug we cannot use emptyValidator currently. The compiler seems to need the input types explicitly defined even if not used.
+                      \(d :: ()) (r :: ()) (v :: ()) -> ()
+                      ||])
 
 scAddress :: Address'
 scAddress = Ledger.scriptAddress walletValidator
